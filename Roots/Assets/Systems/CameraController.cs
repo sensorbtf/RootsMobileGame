cc267f728b4674aea753cb14eb3673387a26176f
Mateusz <mateusz.lyson@blumentmt.com>
2023-09-02 07:12:42 +0200

    Camera Simple Movement

---
+++ Roots/Assets/Systems/CameraController.cs
@@ -0,0 +1,92 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 touchStart;
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 8;
+
+    public Transform leftBoundaryObject;
+    public Transform rightBoundaryObject;
+    public Transform topBoundaryObject;
+    public Transform bottomBoundaryObject;
+
+    public float leftBoundary;
+    public float rightBoundary;
+    public float topBoundary;
+    public float bottomBoundary;
+
+    
+    private void Start()
+    {
+        if (Camera.main != null) 
+            Camera.main.aspect = 9f / 16f; // For a 9:16 ratio
+        
+        if (leftBoundaryObject != null) leftBoundary = leftBoundaryObject.position.x;
+        if (rightBoundaryObject != null) rightBoundary = rightBoundaryObject.position.x;
+        if (topBoundaryObject != null) topBoundary = topBoundaryObject.position.y;
+        if (bottomBoundaryObject != null) bottomBoundary = bottomBoundaryObject.position.y;
+    }
+
+    void Update()
+    {
+        // Camera Movement
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+
+        // Restrict the camera within a boundary box
+        float camHeight = Camera.main.orthographicSize;
+        float camWidth = Camera.main.aspect * camHeight;
+
+        float xMin = leftBoundary + camWidth;
+        float xMax = rightBoundary - camWidth;
+        float yMin = bottomBoundary + camHeight;
+        float yMax = topBoundary - camHeight;
+
+        Vector3 clampedPosition = Camera.main.transform.position;
+        clampedPosition.x = Mathf.Clamp(Camera.main.transform.position.x, xMin, xMax);
+        clampedPosition.y = Mathf.Clamp(Camera.main.transform.position.y, yMin, yMax);
+        Camera.main.transform.position = clampedPosition;
+
+        // Camera Zoom
+        float zoom = Mathf.Clamp(Camera.main.orthographicSize, zoomOutMin, zoomOutMax);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoom, Time.deltaTime);
+
+        // Zoom with Pinch
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            Zoom(difference * 0.01f);
+        }
+
+        // Zoom with Mouse Wheel
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        Zoom(scrollData);
+    }
+
+    void Zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+}
