a7c8e782c588a5f5e3c59b12054a9db2e7ec5a3a
Mateusz <mateusz.lyson@blumentmt.com>
2023-11-09 16:42:04 +0100

    Saving Base Methods

--- Roots/Assets/Systems/GPGS/GPGS.cs
+++ Roots/Assets/Systems/GPGS/GPGS.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
+using GooglePlayGames.BasicApi.SavedGame;
 using TMPro;
 
 public class GPGS : MonoBehaviour
@@ -16,28 +17,67 @@ public class GPGS : MonoBehaviour
 
     public void SignIn()
     {
-        var test = PlayGamesPlatform.Instance;
-        _infoText.text = "Trying" + test;
-        Debug.Log(test);
-        if (test == null)
-        {
-            Debug.Log(test + " IS NULL");
-        }
-        test.Authenticate(ProcessAuthentication);
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
     }
-
+    
     internal void ProcessAuthentication(SignInStatus status)
     {
         if (status == SignInStatus.Success)
         {
-            var name = PlayGamesPlatform.Instance.GetUserDisplayName();
+            var playerName = PlayGamesPlatform.Instance.GetUserDisplayName();
             var id = PlayGamesPlatform.Instance.GetUserId();
             var url = PlayGamesPlatform.Instance.GetUserImageUrl();
-            _infoText.text = $"NAME: {name}. ID {id}. URL {url}";
+            _infoText.text = $"NAME: {playerName}. ID {id}. URL {url}";
         }
         else
         {
             _infoText.text = $"Failed";
+            // local save?
         }
     }
+
+    #region Saving
+
+        void OpenSavedGame(string filename)
+    {
+        ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+        savedGameClient.OpenWithAutomaticConflictResolution(
+            filename,
+            DataSource.ReadCacheOrNetwork,
+            ConflictResolutionStrategy.UseLongestPlaytime,
+            (SavedGameRequestStatus status, ISavedGameMetadata game) => {
+                // handle opening the saved game
+            });
+    }
+
+    void SaveGame(ISavedGameMetadata game, string dataToSave)
+    {
+        byte[] data = System.Text.Encoding.UTF8.GetBytes(dataToSave);
+        SavedGameMetadataUpdate.Builder builder = new SavedGameMetadataUpdate.Builder();
+        SavedGameMetadataUpdate updatedMetadata = builder.Build();
+
+        ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+        savedGameClient.CommitUpdate(game, updatedMetadata, data, (SavedGameRequestStatus status, ISavedGameMetadata updatedGame) => {
+            // handle the result of the save operation
+        });
+    }
+
+    void LoadGameData(ISavedGameMetadata game)
+    {
+        ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+        savedGameClient.ReadBinaryData(game, (SavedGameRequestStatus status, byte[] data) => {
+            if (status == SavedGameRequestStatus.Success)
+            {
+                // handle data (convert from byte[] to string)
+                string savedData = System.Text.Encoding.UTF8.GetString(data);
+                // Now you can use JSON to deserialize savedData to your game data
+            }
+            else
+            {
+                // handle error
+            }
+        });
+    }
+
+    #endregion
 }
